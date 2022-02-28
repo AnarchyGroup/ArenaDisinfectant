b4bca4e057833495230b95d4eafaab1117f41513
KRYMZ0N <jackgrimes05@yahoo.com>
2022-02-28 13:07:47 -0500

    Adding a ton more functionality

--- src/main/java/me/krymz0n/arenadisinfectant/Main.java
+++ src/main/java/me/krymz0n/arenadisinfectant/Main.java
@@ -8,27 +8,33 @@ import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
+import java.util.logging.Logger;
 
 public final class Main extends JavaPlugin {
     private final ArrayList<Block> toRemove = new ArrayList<>();
 
+    private final Logger log = getServer().getLogger();
+
     @Override
     public void onEnable() {
-        removeBlockys();
+        removeBlocks();
+
+        Bukkit.getScheduler().runTaskTimer(this, this::purge, 20L, 1500L);
 
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        purge();
     }
 
-    private void removeBlockys() { // inefficient af,but it's a start
-        int raduis = 150;
-        for (int x = raduis; x >= -raduis; x--) {
+    private void removeBlocks() { // inefficient af,but it's a start
+        log.info("Removing some blocks on startup, this may cause the server to take extra time to load...");
+        int radius = 150;
+        for (int x = radius; x >= -radius; x--) {
             for (int y = 150; y >= 1; y--) {
-                for (int z = raduis; z >= -raduis; z--) {
+                for (int z = radius; z >= -radius; z--) {
                     Block b = Bukkit.getWorld("World").getBlockAt(0, 0, 0);
                     Block newBlock = b.getRelative(x, y, z);
                     if (newBlock.getType() != Material.AIR) {
@@ -39,6 +45,8 @@ public final class Main extends JavaPlugin {
                 }
             }
         }
+
+        log.info("Block removal complete!");
     }
 
     @EventHandler
@@ -49,8 +57,13 @@ public final class Main extends JavaPlugin {
     }
 
     private void purge() {
+        log.info("Starting purge, this can take a few seconds...");
         for (Block b : toRemove) {
-            // more
+            if (b != null && b.getType() != Material.AIR) {
+                b.setType(Material.AIR);
+                toRemove.remove(b);
+            }
         }
+        log.info("Purge complete!");
     }
 }
